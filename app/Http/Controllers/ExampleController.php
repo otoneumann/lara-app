ba691afb0cd13f0ab380bb6f76ce8a1e43b19159
oto <otarshenge@gmail.com>
2023-07-05 21:48:40 +0200

    test

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -8,7 +8,8 @@ class ExampleController extends Controller
 {
     //
     public function homepage(){
-        return '<h1>Homepage!!</h1><a href="/about">view the about page</a>';        
+        return view('homepage');
+               
     }
     public function about(){
         return '<h1>About Page!!</h1><a href="/">Back to home</a>';        
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hi FROM BLADE tEMPLATE</h1>
+
+    <p>A great number is {{ 2 + 2 }}</p>
+    <p>The current year is {{ date('Y') }}</p>
+
+    <a href="/about">Back to about</a>
+</body>
+</html>
