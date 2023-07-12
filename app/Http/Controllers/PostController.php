c349c85c830eb8bd7eb209354b8abdaa614b22e7
oto <oto@otos-Air.fritz.box>
2023-07-12 10:29:51 +0200

    middelware done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    //method viewSinglePost
+    public function viewSinglePost(Post $post){#typehinting->variable must match to routes variable and then magic happens(this will query data using model)
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h3><br>');
+        return view('single-post', ['post' => $post]);
+    }
     //method storeNewPost
     public function storeNewPost(Request $request){
         $incomingFields = $request->validate([
@@ -18,11 +24,18 @@ class PostController extends Controller
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
 
-        Post::create($incomingFields);
-        return 1212121212312312;
+        $newPost =  Post::create($incomingFields);
+        return redirect("/post/{$newPost->id}")->with('success', 'New Post successfuly created');
     }
     // method showCreateForm
     public function showCreateForm(){
+
+    // if(!auth()->check()){
+    //     return redirect('/');
+    // }     
+    
+    
+
         return view('create-post');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,12 @@ use App\Http\Controllers\UserController;
 //Route::get('/aboutyou', [ExampleController::class, "aboutyou"]);
 
 // User related routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('geust');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('mustBeLoggedIn');
 
 // Blog Post related routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');#middelware happening before function works
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
